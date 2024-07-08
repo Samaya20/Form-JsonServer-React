43c3a5dea0f31d465794934ac7819d5daa4a797e
Samaya20 <mughumova@gmail.com>
2024-07-08 16:31:12 +0400

    project v2

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import "../styles/Students.css"
+import "../styles/Students.css";
 
 export default function Students({ students }) {
   return (
     <div className="students">
       {students.map((s) => (
-        <div className="student">
-          <img src={s.image} alt="avatar"/>
-          <h1 key={s.id}>{s.name}</h1>
+        <div key={s.id} className="student">
+          <img src={s.image} alt="avatar" />
+          <div className="text">
+            <h1>
+              {s.name} {s.surname}
+            </h1>
+            <h1>
+              <span>BirthDate : </span>
+              {s.birthdate}
+            </h1>
+            <h1>
+              <span>Score : </span>
+              {s.score}
+            </h1>
+          </div>
         </div>
       ))}
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,3 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-alert(
-  "İlk öncə terminalda  json-server --watch api/db.json  yazmaq lazımdır. Yaddan çıxmasın!"
-);
